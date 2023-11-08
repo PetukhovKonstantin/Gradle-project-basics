67b18c5bb16e9f03ff00347db2f905aceebc742a
Petukhov Konstantin <kostya49496@gmail.com>
2023-11-08 15:47:53 +0600

    Change task2

--- src/main/kotlin/task2/Main.kt
+++ src/main/kotlin/task2/Main.kt
@@ -3,8 +3,8 @@ package basics.task2
 import org.intellij.lang.annotations.RegExp
 
 fun main() {
-    val likes = 61
-    val regex = """[1]${'$'}""".toRegex()
+    val likes = 101
+    val regex = """([^1]+|^)[1]${'$'}""".toRegex()
     val peopleWord = if (regex.containsMatchIn(likes.toString())) "человеку" else "людям"
 
     println("Понравилось ${likes.toString()} $peopleWord")
